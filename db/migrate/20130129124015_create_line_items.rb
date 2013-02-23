01708cd643ceb93abb072c203684999fe1b35984
Alexey <alexey.kondratenko@mail.ru>
2013-02-23 21:57:22 +0200

    add db folder

---
+++ db/migrate/20130129124015_create_line_items.rb
@@ -0,0 +1,12 @@
+class CreateLineItems < ActiveRecord::Migration
+  def change
+    create_table :line_items do |t|
+	  t.belongs_to :product
+      t.belongs_to :cart
+      t.string :product_id
+      t.integer :cart_id
+
+      t.timestamps
+    end
+  end
+end
